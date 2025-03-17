ccf2169d6ade0f434ed3dc0b3c79650876c878c5
maxpdr96 <maxpdr96@gmail.com>
2025-03-17 09:38:03 -0300

    Ajuste no QuartzConfig

--- src/main/java/com/hidarisoft/springbatchstudy/config/QuartzConfig.java
+++ src/main/java/com/hidarisoft/springbatchstudy/config/QuartzConfig.java
@@ -3,99 +3,76 @@ package com.hidarisoft.springbatchstudy.config;
 import com.hidarisoft.springbatchstudy.quartz.FirstQuartzJobBean;
 import com.hidarisoft.springbatchstudy.quartz.SecondQuartzJobBean;
 import com.hidarisoft.springbatchstudy.quartz.ThirdQuartzJobBean;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.quartz.Job;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
 import org.quartz.SimpleScheduleBuilder;
 import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 @Configuration
 @Slf4j
 @RequiredArgsConstructor
 public class QuartzConfig {
-    public record Tuple<A, B>(A first, B second) {
-    }
 
-    private final Scheduler scheduler;
-
-    @PostConstruct
-    public void init() throws SchedulerException {
-        var first = firstJobExecuted();
-        var second = secondJobExecuted();
-        var third = thirdJobExecuted();
-
-        Map<JobDetail, Set<? extends Trigger>> map = new HashMap<>();
-        map.put(first.first, Set.of(first.second));
-        map.put(second.first, Set.of(second.second));
-        map.put(third.first, Set.of(third.second));
-        scheduler.scheduleJobs(map, false);
-    }
-
-    private Tuple<JobDetail, Trigger> firstJobExecuted() {
-        log.info("First init");
-
-        JobDetail firstJobDetail = JobBuilder.newJob(FirstQuartzJobBean.class)
-                .withIdentity("firstJobDetail", "settleBatch")
+    @Bean
+    public JobDetail quartzJobDetailFirstJob() {
+        return JobBuilder.newJob(FirstQuartzJobBean.class)
+                .storeDurably()
                 .build();
+    }
 
-        Trigger firstTrigger = TriggerBuilder
+    @Bean
+    public Trigger firstJobExecuted() {
+        return TriggerBuilder
                 .newTrigger()
-                .forJob(firstJobDetail)
-                .withIdentity("settleDaysTrigger", "settleBatch")
+                .forJob(quartzJobDetailFirstJob())
                 .withSchedule(SimpleScheduleBuilder.simpleSchedule()
                         .withIntervalInMinutes(1)
                         .repeatForever())
                 .build();
-
-        return new Tuple<>(firstJobDetail, firstTrigger);
     }
 
-    private Tuple<JobDetail, Trigger> secondJobExecuted() {
-        log.info("Second init");
 
-        JobDetail secondJobDetail = JobBuilder.newJob(SecondQuartzJobBean.class)
-                .withIdentity("secondJobDetail", "settleBatch")
+    @Bean
+    public JobDetail quartzJobDetailSecondJob() {
+        return JobBuilder.newJob(SecondQuartzJobBean.class)
+                .storeDurably()
+                .storeDurably()
                 .build();
+    }
 
-        Trigger secondTrigger = TriggerBuilder
+    @Bean
+    public Trigger secondJobExecuted() {
+
+        return TriggerBuilder
                 .newTrigger()
-                .forJob(secondJobDetail)
-                .withIdentity("settleDaysTrigger", "settleBatch")
+                .forJob(quartzJobDetailSecondJob())
                 .withSchedule(SimpleScheduleBuilder.simpleSchedule()
                         .withIntervalInMinutes(2)
                         .repeatForever())
                 .build();
 
-        return new Tuple<>(secondJobDetail, secondTrigger);
     }
 
-    private Tuple<JobDetail, Trigger> thirdJobExecuted() {
-        log.info("Third init");
-
-        JobDetail thirdJobDetail = JobBuilder.newJob(ThirdQuartzJobBean.class)
-                .withIdentity("thirdJobDetail", "settleBatch")
+    @Bean
+    public JobDetail quartzJobDetailThirdJob() {
+        return JobBuilder.newJob(ThirdQuartzJobBean.class)
+                .storeDurably()
                 .build();
+    }
 
-        Trigger thirdTrigger = TriggerBuilder
+    @Bean
+    public Trigger thirdJobExecuted() {
+        return TriggerBuilder
                 .newTrigger()
-                .forJob(thirdJobDetail)
-                .withIdentity("settleDaysTrigger", "settleBatch")
+                .forJob(quartzJobDetailThirdJob())
                 .withSchedule(SimpleScheduleBuilder.simpleSchedule()
                         .withIntervalInMinutes(3)
                         .repeatForever())
                 .build();
-
-        return new Tuple<>(thirdJobDetail, thirdTrigger);
     }
 }
